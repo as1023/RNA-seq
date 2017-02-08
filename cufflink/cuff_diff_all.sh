4657c91ea8639869f43c715df74983a90eda1d6a
python_tutorial <amitbiochem.iitm@gmail.com>
2017-02-08 23:50:44 +0100

    Update cuff_diff_all.sh

--- cufflink/cuff_diff_all.sh
+++ cufflink/cuff_diff_all.sh
@@ -12,7 +12,7 @@ echo "SLURM_JOB_NODELIST"=$SLURM_JOB_NODELIST
 echo "SLURM_NNODES"=$SLURM_NNODES
 echo "working directory = "$SLURM_SUBMIT_DIR
 
-index=/prj/leo_kurian_killifish/Amit/final/merged_asm
-out=/prj/leo_kurian_killifish/Amit/final
+index=/prj/as1023/final/merged_asm
+out=/prj/data/as1023/final
 cuffdiff -p 20 -C contrast.txt --min-reps-for-js-test 2 --dispersion-method per-condition --output-dir ${out}/cuffdiff_All --library-type fr-firststrand --use-sample-sheet ${index}/merged.gtf sample_sheet_final.txt 
 
